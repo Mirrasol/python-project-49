57cf55b76a66c6b91341724387297f67ebb3a4ef
Elena Dobruk <elena.dobruk@yandex.ru>
2023-12-19 18:04:39 +0300

    step9: add games/prime.py, add scrpts/brain_prime.py, change pyproject.toml

---
+++ brain_games/games/prime.py
@@ -0,0 +1,18 @@
+import random
+
+
+def game_prime():
+    num = random.randint(1, 200)
+    expression = num
+    correct_answer = "yes"
+    if num == 1:
+        correct_answer = "no"
+    else:
+        for i in range(2, int(num ** 0.5 + 1)):
+            if num % i == 0:
+                correct_answer = "no"
+                break
+    return expression, correct_answer
+
+
+statement = "Answer 'yes' if given number is prime. Otherwise answer 'no'."
